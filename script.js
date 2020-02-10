e695bc26f8eb33c02cc281396d381df20efbf7c3
Thabang Moeketsi <codeconstable@gmail.com>
2020-02-10 14:29:51 +0200

    Initial Commit

--- script.js
+++ script.js
@@ -13,44 +13,6 @@ $(document).ready(function () {
   });
 });
 
-$('owl-carousel').owlCarousel({
-  margin: 10,
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-
-    680: {
-      items: 2
-    },
-
-    960: {
-      items: 3
-    }
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
